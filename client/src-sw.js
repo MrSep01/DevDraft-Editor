9a07c3c848e3edfb071a3432697f9b7c58d9768b
sep <sep.alamouti@sepalamouti.com>
2023-12-04 10:29:10 -0800

    deleted-the-wrong-starting-code

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,49 +1,30 @@
-const butInstall = document.getElementById('buttonInstall');
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-// Before install prompt event
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent the mini-infobar from appearing on mobile
-  event.preventDefault();
-  
-  // Stash the event so it can be triggered later.
-  window.deferredPrompt = event;
-  
-  // Remove the 'hidden' class from the button.
-  butInstall.classList.remove('hidden');
+precacheAndRoute(self.__WB_MANIFEST);
+
+const pageCache = new CacheFirst({
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
 });
 
-// Install button click event
-butInstall.addEventListener('click', async () => {
-  const promptEvent = window.deferredPrompt;
-  
-  if (!promptEvent) {
-    return;
-  }
-  
-  // Show the install prompt
-  promptEvent.prompt();
-  
-  // Wait for the user to respond to the prompt
-  const { outcome } = await promptEvent.userChoice;
-  
-  // Optionally, send analytics event with outcome of user choice
-  console.log(`User response to the install prompt: ${outcome}`);
-  
-  // We've used the prompt, and can't use it again, discard it
-  window.deferredPrompt = null;
-  
-  // Hide the install button
-  butInstall.classList.add('hidden');
+warmStrategyCache({
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
 });
 
-// App installed event
-window.addEventListener('appinstalled', (event) => {
-  // Hide the install button
-  butInstall.classList.add('hidden');
-  
-  // Clear the deferredPrompt so it can be garbage collected
-  window.deferredPrompt = null;
-  
-  // Optionally, send analytics event to indicate successful installation
-  console.log('PWA was installed');
-});
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+// TODO: Implement asset caching
+registerRoute();

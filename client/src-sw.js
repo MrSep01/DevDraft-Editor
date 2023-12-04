28a83b410ac25ab2a3b21fc6ed935627f9a8fd44
sep <sep.alamouti@sepalamouti.com>
2023-12-04 10:52:12 -0800

    srw.js-update

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,55 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+// const { warmStrategyCache } = require('workbox-recipes')
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+cacheName: 'page-cache',
+plugins: [
+  new CacheableResponsePlugin({
+    statuses: [0, 200],
+  }),
+  new ExpirationPlugin({
+    maxAgeSeconds: 30 * 24 * 60 * 60,
+  }),
+],
+});
+
+warmStrategyCache({
+urls: ['/index.html', '/'],
+strategy: pageCache,
+});
+
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+
+// Asset Caching: CSS, JS, Worker Scripts
+registerRoute(
+// Define the function that determines which requests to cache
+({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+new StaleWhileRevalidate({
+  cacheName: 'asset-cache',
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
     new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
+      maxEntries: 50,
+      maxAgeSeconds: 365 * 24 * 60 * 60, // 1 year
     }),
   ],
+})
+);
+
+// Implementing offlineFallback
+offlineFallback({
+  pageFallback: '/offline.html',
+  // Add other fallbacks as necessary
 });
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-
-// TODO: Implement asset caching
-registerRoute();

7f70d2e1f38661a0f2a520be01c52b815dba60c4
sep <sep.alamouti@sepalamouti.com>
2023-12-04 10:37:30 -0800

    updated-webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,13 +15,63 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Configuring HtmlWebpackPlugin to inject bundled scripts into our HTML
+      new HtmlWebpackPlugin({
+        template: './index.html', // Path to your source index.html
+        title: 'Editor',
+      }),
+
+
+  // Injects our custom service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+
+      // Generates a manifest.json for our PWA
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'DevDraft Editor',
+        short_name: 'DevDraft',
+        description: 'An offline editor for DevDraft',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        // Rule for CSS files
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'], // Use these loaders for CSS files
+        },
+        // Rule for JavaScript files
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'], // Babel presets
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+
+            },
+          },
+        },
       ],
     },
   };
 };
+
